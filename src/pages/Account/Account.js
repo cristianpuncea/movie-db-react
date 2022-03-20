afa125ffe4f488cf4a6145c366c2566871200fe5
Cristian Puncea <cristian.puncea@gmail.com>
2022-03-20 20:57:37 +0200

    Implementing login functionality with reqres.in

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Account() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    };
+  }, []);
+
   return (
     <div>
       Account Page.
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,66 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const savedToken = localStorage.getItem("token");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem("token", data.token);
+        }
+        navigate("/account");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    if (savedToken) {navigate("/account")}
+  })
+
   return (
-   <div>
-     Login Page.
-   </div>
+    <Row className="justify-content-center py-4">
+      <Col sm="auto">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="success" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   );
 }
